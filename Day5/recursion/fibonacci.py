9623c3c25b66f2785c44658cbf9a6c809a3064ba
PadmavathyNarayanan <36335169+PadmavathyNarayanan@users.noreply.github.com>
2026-03-04 09:23:45 +0530

    Create fibonacci.py

---
+++ Day5/recursion/fibonacci.py
@@ -0,0 +1,12 @@
+'''
+fibonacci series 5
+0 1 1 2 3 5
+'''
+
+def fibonacci(n):
+    if n <= 1:
+        return n
+    return fibonacci(n-1) + fibonacci(n-2)
+
+for i in range(6):
+    print(fibonacci(i))
